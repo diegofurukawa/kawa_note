4118795d14351d143d3c38dfd5b37bc4be8bead7
DIEGO FURUKAWA <53219860+diegofurukawa@users.noreply.github.com>
2026-02-19 19:40:08 -0300

    FIX - TOKEN CREDENTIALS

--- src/components/onboarding/hooks/useUserCredentialsForm.js
+++ src/components/onboarding/hooks/useUserCredentialsForm.js
@@ -50,7 +50,7 @@ export function useUserCredentialsForm(tenantId, onSuccess, onError) {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
+          'Authorization': `Bearer ${localStorage.getItem('kawa_access_token')}`
         },
         body: JSON.stringify({
           name: data.name,
